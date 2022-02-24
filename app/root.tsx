e93b6d841a2837938272249f7da8eb7f448f1f8d
Ryan O. Mackey <ryan.o.mackey@gmail.com>
2022-02-23 20:24:03 -0600

    update default favicon

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,10 @@ import {
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
-  return { title: "did you get a haircut?" };
+  return { 
+    title: "did you get a haircut?",
+    description: "if you're currently wondering if I, specifically, got a haircut recently"
+  };
 };
 
 export default function App() {
@@ -18,6 +21,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💇🏻‍♂️</text></svg>" />
         <Meta />
         <Links />
       </head>
